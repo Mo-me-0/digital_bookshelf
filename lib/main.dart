d0e126a2584fa2110c91169633fbc572ccbd0d26
Joniman478 <146421273+Joniman478@users.noreply.github.com>
2026-04-08 01:34:54 -0700

    file picker logic

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:file_picker/file_picker.dart';
 
 void main() => runApp(const MyApp());
 
@@ -11,24 +12,58 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'My Bookshelf',
       theme: ThemeData(
-        colorScheme: .fromSeed(seedColor: Colors.brown),
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.brown),
       ),
       home: const HomePage(title: 'My Bookshelf'),
     );
   }
 }
 
-class HomePage extends StatelessWidget{
+class HomePage extends StatefulWidget {
   final String title;
   const HomePage({super.key, required this.title});
-  
+
   @override
-  
-  Widget build(BuildContext context){
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  String? _selectedFileName;
+
+  Future<void> _pickFile() async {
+    FilePickerResult? result = await FilePicker.platform.pickFiles();
+
+    if (result != null) {
+      setState(() {
+        _selectedFileName = result.files.single.name;
+      });
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        title: Text(title),
+        title: Text(widget.title),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(
+              _selectedFileName != null
+                  ? 'Selected File: $_selectedFileName'
+                  : 'No file selected',
+              style: Theme.of(context).textTheme.bodyLarge,
+            ),
+            const SizedBox(height: 20),
+            ElevatedButton(
+              onPressed: _pickFile,
+              child: const Text('Pick a File'),
+            ),
+          ],
+        ),
       ),
     );
   }
